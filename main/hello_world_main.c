2e77957126680a478b01217aeee7344a9b17834e
Ehsan Khodadad <ehkh@dtu.dk>
2025-12-28 16:35:42 +0100

    simplest

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -7,22 +7,12 @@
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "esp_system.h"
-#include "esp_rom_sys.h"
-
-static inline void delay_ms(uint32_t ms)
-{
-    esp_rom_delay_us((int)ms * 1000);
-}
 
 void app_main(void)
 {
     printf("Hello world!\n");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        delay_ms(1000);
+    while(1) {
+        // Infinite loop
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
